8cc144c575468a1280e627f355c1067e1ace6402
Goldside543 <goldside000@outlook.com>
2025-05-08 01:25:13 -0400

    Create crt0.s

---
+++ crt0.s
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: LGPL-2.1-only
+
+.text
+
+.globl _start
+
+_start: # _start is the entry point known to the linker
+    xor %ebp, %ebp            # RBP := 0, because we like tidy stack frames
+    mov (%esp), %eax          # Get argc from the stack
+    lea 4(%esp), %ecx         # Get the address of argv
+    lea 4(%ecx,%eax,4), %edx  # Get the address of envp
+    xor %ebx, %ebx            # Clear EBX because it's polite, and also ABI-compliant
+
+    push %edx                 # Push envp
+    push %ecx                 # Push argv
+    push %eax                 # Push argc
+    call main                 # Call main(argc, argv, envp)
+
+    mov %eax, %ebx            # Transfer main's return value into EBX
+    xor %eax, %eax            # Clear EAX for syscall cleanliness
+    movl $6, %eax             # Exit syscall is 6
+    int $0x80                 # Call _exit via syscall
