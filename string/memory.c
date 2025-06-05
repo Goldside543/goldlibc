d14417ad50ff3ec94760d662d5f8fb06b9c6f0b1
Goldside543 <goldside000@outlook.com>
2025-06-05 01:16:40 -0400

    Create memory.c

---
+++ string/memory.c
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * string/memory.c
+ * 
+ * Memory related functions.
+ *
+ * Copyright (C) 2025 Goldside543
+ *
+ */
+
+void* memset(void* ptr, int value, size_t num) {
+    unsigned char* p = (unsigned char*)ptr;
+    while (num--) {
+        *p++ = (unsigned char)value;
+    }
+    return ptr;
+}
+
+void* memcpy(void* dest, const void* src, size_t num) {
+    unsigned char* d = (unsigned char*)dest;
+    const unsigned char* s = (const unsigned char*)src;
+    while (num--) {
+        *d++ = *s++;
+    }
+    return dest;
+}
+
+int memcmp(const void* ptr1, const void* ptr2, size_t num) {
+    const unsigned char* p1 = (const unsigned char*)ptr1;
+    const unsigned char* p2 = (const unsigned char*)ptr2;
+    for (size_t i = 0; i < num; ++i) {
+        if (p1[i] != p2[i]) {
+            return p1[i] - p2[i];
+        }
+    }
+    return 0;
+}
