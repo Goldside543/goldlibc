cba3ce2485ba5a1e3ba5c013c2059d9ca5ec217d
Goldside543 <goldside000@outlook.com>
2025-06-05 01:32:12 -0400

    Update crt0.s

--- crt0.s
+++ crt0.s
@@ -5,13 +5,13 @@
 .globl _start
 
 _start: # _start is the entry point known to the linker
+    call init_heap            # Initialize the heap
+
     xor %ebp, %ebp            # EBP := 0, because we like tidy stack frames
     movl (%esp), %eax          # Get argc from the stack
     lea 4(%esp), %ecx         # Get the address of argv
     lea 4(%ecx,%eax,4), %edx  # Get the address of envp
     xor %ebx, %ebx            # Clear EBX because it's polite, and also ABI-compliant
-
-    call init_heap            # Initialize the heap
 
     pushl %edx                 # Push envp
     pushl %ecx                 # Push argv
