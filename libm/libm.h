189df9135cabc99221226b9de57ddcb0676a7a8b
Goldside543 <goldside000@outlook.com>
2025-11-24 21:52:05 -0500

    Create libm.h

---
+++ libm/libm.h
@@ -0,0 +1,16 @@
+#ifndef LIBM_H
+#define LIBM_H
+
+double fabs(double x);
+double floor(double x);
+double ceil(double x);
+double fmod(double x, double y);
+double sqrt(double x);
+double exp(double x);
+double log(double x);
+double pow(double x, double y);
+double sin(double x);
+double cos(double x);
+double tan(double x);
+
+#endif /* LIBM_H */
